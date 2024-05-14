d0ef8083ff3a6720cf09fd5d15272035f3e5c0fe
FALCH2000 <faleiva.ch@gmail.com>
2024-05-14 16:34:17 -0600

    updates

--- Frontend/FrontendMyRest/src/app/user/reservation/reservation.component.ts
+++ Frontend/FrontendMyRest/src/app/user/reservation/reservation.component.ts
@@ -3,6 +3,8 @@ import { ReservationService } from '../services/reservation.service';
 import { HttpClient } from '@angular/common/http';
 
 import {FormBuilder} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-reservation',
@@ -16,24 +18,55 @@ export class ReservationComponent {
   selectedDateFormated = ""
   selectedDay = ""
   selectedTable = ""
+  numberOfPeople = ""
+
+  table_selected_list:any[] = [];
 
   tables_selected = this._formBuilder.group({});
 
+  userToken:string|null;
+  userVerification:boolean = false;
 
-  constructor(private reservationService:ReservationService, private http: HttpClient, private _formBuilder: FormBuilder){}
+
+  constructor(private reservationService:ReservationService, private http: HttpClient,
+     private _formBuilder: FormBuilder, private router: Router){
+      this.userToken = localStorage.getItem('token')
+     }
 
   ngOnInit(): void {}
 
-
+  verifyUser(){
+    if(this.userToken === null){
+      alert("Debes loguearte nuevamente para completar la tarea")
+      this.router.navigate(['homepage']);
+    }else{
+      this.reservationService.userIsValid(this.userToken).subscribe((data)=>{
+        console.log(data)
+        if(data.status === 200){
+          this.userVerification = true;
+          this.searchTable()
+          
+        }else{
+          alert("Debes loguearte nuevamente para completar la tarea")
+          this.router.navigate(['homepage']);
+        }
+        return true
+      })
+    }
+  }
 
   searchTable(): void {
+
     const selectedDate = new Date(this.selectedDay);
+    this.table_selected_list = []
 
     if (!isNaN(selectedDate.getTime())) {
+
       const year = selectedDate.getFullYear();
       const month = ('0' + (selectedDate.getMonth() + 1)).slice(-2); 
       const day = ('0' + selectedDate.getDate()).slice(-2);
       this.selectedDateFormated = `${year}-${month}-${day}`;
+
       this.reservationService.getCalendar(this.selectedDateFormated,this.selectedHour+":00").subscribe((data)=>{
         if(data.status === 200){
           console.log(data.data.available_tables)
@@ -48,16 +81,52 @@ export class ReservationComponent {
   }
 
   reserve(){
-
     // Obtener el valor de todas las mesas
     this.selected_tables.forEach((mesa: { Table_ID: any; Chairs: any; }) => {
       const Table_ID = mesa.Table_ID;
       const Chairs = mesa.Chairs;
+      this.table_selected_list.push(mesa.Table_ID)
       console.log(`ID de la mesa: ${Table_ID}, Sillas: ${Chairs}`);
     }); 
 
+    if(this.userToken === null){
+      alert("Vuelve a loguearte para poder hacer esta petición")
+    } else{
+      const registro = {
+        "token": this.userToken,
+        "data":{
+          "method": "crear-reserva",
+          "number_of_people": String(this.numberOfPeople),
+          "reservation_date": this.selectedDateFormated,
+          "start_time": this.selectedHour,
+          "selected_tables": this.table_selected_list
+        }
+      }
+      console.log(registro);
+      this.sendRequest(registro)
+    }
     
-    
+  }
+
+  sendRequest(registro:any){
+    const observer: Observer<any> = {
+      next: (data: any) => {
+        alert(`La reserva fue creada y la respuesta del servidor fue:\n${data.response}`)
+        this.clear()
+        this.router.navigate(['user/reservation']);
+        console.log(data);
+      },
+      error: (error: any) => {
+        console.error('Error occurred:', error);
+        alert("Hubo un error creando el usuario. Vuelve a intentarlo")
+      },
+      complete: function (): void {
+        throw new Error('Function not implemented.');
+      }
+    };
+
+    this.reservationService.createReservation(registro).subscribe(observer);
+
   }
 
   onCheckboxChange(event: any, table: any): void {
@@ -77,5 +146,21 @@ export class ReservationComponent {
   goBack(){
     this.selectedHour = "";
     this.selectedDay = "";
+  }
+
+  clear(){
+    this.available_tables = new Array<any>();
+    this.selected_tables = new Array<any>();
+    this.selectedHour = ""
+    this.selectedDateFormated = ""
+    this.selectedDay = ""
+    this.selectedTable = ""
+    this.numberOfPeople = ""
+
+    this.table_selected_list = [];
+
+    this.tables_selected = this._formBuilder.group({});
+
+    this.userVerification = false;
   }
 }
--- Frontend/FrontendMyRest/src/app/user/services/reservation.service.ts
+++ Frontend/FrontendMyRest/src/app/user/services/reservation.service.ts
@@ -1,6 +1,7 @@
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
+import {catchError} from 'rxjs/operators'; 
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,29 @@ export class ReservationService {
     return this.http.get<any>(url);
   }
 
-  editReservation(reservation:any):Observable<any>{
+  createReservation(reservation:any):Observable<any>{
     var url = 'https://us-central1-soa-project3.cloudfunctions.net/broker'
-    return this.http.post<any>(url, reservation);
+    return this.http.post<any>(url, reservation).pipe(catchError(this.handleError));
+  }
+
+  userIsValid(userToken:any):Observable<any>{
+    var url = `https://us-central1-soa-project3.cloudfunctions.net/verificar-usuario/?token=${userToken}`
+    return this.http.get<any>(url);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 
 }
